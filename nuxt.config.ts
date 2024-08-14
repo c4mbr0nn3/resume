8c1ff8c9ac655addfe67e504f7883bc7741798e0
Francesco Zorzi <frensinik89@yahoo.it>
2024-08-14 11:01:43 +0200

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/image"]
+  modules: ["@nuxt/image", "@nuxtjs/tailwindcss"],
+
+  tailwindcss: {
+    viewer: false,
+  },
 })
