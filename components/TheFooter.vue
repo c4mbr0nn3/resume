572798444fe328a8e890055c617479f1a7b2eabb
Francesco Zorzi <frensinik89@yahoo.it>
2024-08-14 19:46:21 +0200

    feat: add thanks in the footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -9,15 +9,24 @@ const copyright = computed(() => {
 </script>
 
 <template>
-  <footer class=" text-center text-slate-500 font-light p-10 flex items-center justify-center gap-3">
-    <p>{{ copyright }} </p>
-    -
-    <p class="flex items-center">
-      You can find the source code on&nbsp;
-      <NuxtLink to="https://gitlab.com/j1mm0/resume" class="flex items-center gap-1 text-emerald-600 hover:text-orange-600 transition-all">
-        GitLab
-        <Icon name="simple-icons:gitlab" />
+  <footer class=" text-center text-slate-500 font-light pt-10">
+    <div class="flex items-center justify-center gap-3">
+      <p>{{ copyright }} </p>
+      -
+      <p class="flex items-center">
+        You can find the source code on&nbsp;
+        <NuxtLink to="https://gitlab.com/j1mm0/resume" class="flex items-center gap-1 text-emerald-600 hover:text-orange-600 transition-all">
+          GitLab
+          <Icon name="simple-icons:gitlab" />
+        </NuxtLink>
+      </p>
+    </div>
+    <div class="text-xs mt-2">
+      Thanks to
+      <NuxtLink to="https://nathanfriend.io" class="text-emerald-600 hover:text-orange-600 transition-allte">
+        Nathan Friend
       </NuxtLink>
-    </p>
+      for the inspiration!
+    </div>
   </footer>
 </template>
