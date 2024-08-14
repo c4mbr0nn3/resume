243d6ee9c61f3d616a5fa4d249c20bbe913f160e
Francesco Zorzi <frensinik89@yahoo.it>
2024-08-14 16:49:21 +0200

    chore: switch back to npm, add nuxt fonts, add nuxt icon, add basic head info

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss", '@vueuse/nuxt'],
+  devtools: { enabled: false },
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+  ],
+
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Francesco Zorzi - Resume',
+      meta: [
+        { name: 'description', content: 'Francesco Zorzi - Resume' },
+      ],
+    },
+  },
 
   tailwindcss: {
-    viewer: false,
+    config: {
+      theme: {
+        container: {
+          center: true,
+        },
+        extend: {
+          fontFamily: {
+            serif: ['Merriweather', 'serif'],
+            sans: ['Inter', 'sans-serif'],
+            mono: ['Source Code Pro', 'monospace'],
+          },
+        },
+      },
+    },
   },
 })
