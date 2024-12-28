bd631281f051722240d802526abd2ce533b9cd0d
Francesco Zorzi <frensinik89@yahoo.it>
2024-12-28 09:41:08 +0100

    build: try to force netlify-static preset for nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'netlify-static',
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: [
