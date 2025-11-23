cb1612761c3ff3c71e703a67e5f979846325b848
Francesco Zorzi <frensinik89@yahoo.it>
2025-11-23 16:33:25 +0100

    feat: enhance resume summary and structure components

--- app/components/TheResume.vue
+++ app/components/TheResume.vue
@@ -2,6 +2,7 @@
   <div class="w-full bg-white | sm:rounded-xl sm:shadow-lg">
     <div class="flex flex-col gap-5 p-5 | sm:p-10 sm:gap-10">
       <ResumeHeader />
+      <ResumeSummary />
       <ResumeContent />
     </div>
   </div>
---
+++ app/components/resume/summary.vue
@@ -0,0 +1,9 @@
+<script setup>
+const { basics } = useResume()
+</script>
+
+<template>
+  <section v-if="basics?.summary">
+    <Markdown :content="basics.summary" />
+  </section>
+</template>
