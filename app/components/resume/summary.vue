d84b6035e4fe1750c62146dabed12bab1a7546c6
Francesco Zorzi <frensinik89@yahoo.it>
2026-04-19 11:32:50 +0200

    chore: migrate to astro

--- app/components/resume/summary.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-const { basics } = useResume()
-</script>
-
-<template>
-  <section v-if="basics?.summary">
-    <Markdown :content="basics.summary" />
-  </section>
-</template>
--- app/pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container | sm:p-10 lg:p-20">
-    <TheResume />
-    <TheFooter />
-  </div>
-</template>
